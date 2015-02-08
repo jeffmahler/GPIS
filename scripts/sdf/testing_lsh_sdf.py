7efb1790266fe488767c34a93ac7c8516bb5399f
Sahaana Suri <sahaana.suri@gmail.com>
2015-02-08 13:40:27 -0800

    adding storage option

--- scripts/sdf/testing_lsh_sdf.py
+++ scripts/sdf/testing_lsh_sdf.py
@@ -1,10 +1,17 @@
+import pickle
 from engine_creation import train_and_test_lsh
 
 NUM_TRAIN = 50
 NUM_TEST = 3
 ROOT_DIR = "datasets/Cat50_ModelDatabase"
 
+SAVE_ENGINE = True
+SAVE_LOCATION = "testing_lsh_storage.pk1"
+
 accuracy, engine, results = train_and_test_lsh(NUM_TRAIN, NUM_TEST, ROOT_DIR)
+if SAVE_ENGINE:
+    with open(SAVE_LOCATION, 'wb') as output:
+        pickle.dump(engine, output, pickle.HIGHEST_PROTOCOL)
 
 print "Accuracy: %s"%(accuracy)
 
