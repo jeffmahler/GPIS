Finished conversion of grasps to PR2 tools frame and OpenRave. Also added keys for graspable objects, fixed sdf centroid bug, and added sample caching to the pfc calculation. Antipodal and pr2_grasp_checker need to be cleaned up

src/grasp_selection/antipodal_grasp_sampler.py
@@ -16,6 +16,21 @@ import sdf_file
 
 import IPython
 
+# basically a bucket for todo vis code, don't want to lose it
+def vis_graspable_grasp(graspable, grasp):
+
+    graspable.mesh.visualize()
+    mv.axes()
+
+    x, v = graspable.sdf.surface_points(grid_basis = False)
+    mv.points3d(x[:,0], x[:,1], x[:,2], scale_factor = 0.01)
+
+    g1, g2 = grasp.endpoints()
+    mv.points3d(g1[0], g1[1], g1[2], scale_factor = 0.01, color=(0.5,0.5,0.5))                        
+    mv.points3d(g2[0], g2[1], g2[2], scale_factor = 0.01, color=(0.5,0.5,0.5))                        
+    mv.points3d(grasp.center[0], grasp.center[1], grasp.center[2], scale_factor = 0.01, color=(0.5,0.5,0.5))                        
+    mv.show()
+
 class AntipodalGraspParams:
     """ Struct to hold antipodal grasp with statistics """
     def __init__(self, obj, grasp, alpha1, alpha2, rho1, rho2):
@@ -35,12 +50,12 @@ class AntipodalGraspSampler(object):
         self.grasp_width = config['grasp_width']
         self.friction_coef = config['friction_coef']
         self.num_cone_faces = config['num_cone_faces']
-        self.num_samples = config['num_samples']
+        self.num_samples = config['grasp_samples_per_surface_point']
         self.dir_prior = config['dir_prior']
-        self.alpha_thresh = config['alpha_thresh']
+        self.alpha_thresh = config['alpha_thresh'] * np.pi
         self.rho_thresh = config['rho_thresh']
-        self.vis = config['vis_antipodal']
         self.min_num_grasps = config['min_num_grasps']
+        self.theta_res = config['grasp_theta_res'] * np.pi
         self.alpha_inc = config['alpha_inc']
         self.rho_inc = config['rho_inc']
 
@@ -91,6 +106,14 @@ class AntipodalGraspSampler(object):
         ap_grasps = []
         surface_points, _ = graspable.sdf.surface_points(grid_basis=False)
 
+        # load openrave
+        rave.raveSetDebugLevel(rave.DebugLevel.Error)
+        e = rave.Environment()
+        e.Load(pgc.PR2_MODEL_FILE)
+        e.SetViewer("qtcoin")
+        r = e.GetRobots()[0]
+        grasp_checker = pgc.PR2GraspChecker(e, r, mesh_name)
+
         for x1 in surface_points:
             start_time = time.clock()
 
@@ -100,8 +123,8 @@ class AntipodalGraspSampler(object):
                 x1 = x1 + (graspable.sdf.resolution / 2.0) * (np.random.rand(3) - 0.5)
 
                 # compute friction cone faces
-                cone_succeeded, cone1, n1 = \
-                    graspable.contact_friction_cone(x1, num_cone_faces = self.num_cone_faces, friction_coef = self.friction_coef)
+                cone_succeeded, cone1, n1 = graspable.contact_friction_cone(x1, num_cone_faces = self.num_cone_faces,
+                                                                            friction_coef = self.friction_coef)
                 if not cone_succeeded:
                     continue
                 cone_time = time.clock()
@@ -160,19 +183,12 @@ class AntipodalGraspSampler(object):
                         antipodal_grasp = AntipodalGraspParams(graspable, grasp, alpha1, alpha2, rho1, rho2)
                         ap_grasps.append(antipodal_grasp)
 
+                        #cf, c = grasp.close_fingers(graspable, vis = True)
+                        #grasp_checker.prune_grasps_in_collision([grasp], vis = True)
+
                         #logging.error('Cone time: %f' %(cone_time - start_time))
                         #logging.error('Sample time: %f' %(sample_time - cone_time))
                         #logging.error('Within cone time: %f' %(within_cone_time - sample_time))
-
-        # load openrave
-        """
-        rave.raveSetDebugLevel(rave.DebugLevel.Error)
-        e = rave.Environment()
-        e.Load(pgc.PR2_MODEL_FILE)
-        e.SetViewer("qtcoin")
-        r = e.GetRobots()[0]
-        grasp_checker = pgc.PR2GraspChecker(e, r, mesh_name)
-        """
 
         # go back through grasps and threshold            
         grasps = []
@@ -181,20 +197,29 @@ class AntipodalGraspSampler(object):
         while len(grasps) < self.min_num_grasps:
             # prune grasps above thresholds
             grasps = []
-#            pr2_grasps = []
+            pr2_grasps = []
             for ap_grasp in ap_grasps:
                 if max(ap_grasp.alpha1, ap_grasp.alpha2) < alpha_thresh and \
                         max(ap_grasp.rho1, ap_grasp.rho2) < rho_thresh:
                     # convert grasps to PR2 gripper poses
                     grasps.append(ap_grasp.grasp)
-#                    pr2_grasps.extend(ap_grasp.grasp.pr2_gripper_poses(graspable))
+                    rotated_grasps = ap_grasp.grasp.transform(graspable.tf, self.theta_res)
+                    pr2_grasps.extend(rotated_grasps)
+
+                    """
+                    ap_grasp.grasp.close_fingers(graspable, vis = True)
+                    grasp_checker.prune_grasps_in_collision(rotated_grasps, vis = True, auto_step = True, close_fingers = True)
+                    IPython.embed()
+                    """
 
             # prune grasps in collision (TODO)
-#            pr2_grasps = grasp_checker.prune_grasps_in_collision(pr2_grasps, vis = True)
 
             # update alpha and rho thresholds
             alpha_thresh = alpha_thresh * self.alpha_inc
             rho_thresh = alpha_thresh * self.rho_inc
+
+        # display pr2 grasps - not currently used
+        pr2_grasps = grasp_checker.prune_grasps_in_collision(pr2_grasps, vis = True, auto_step = True)
     
         """
         for grasp in grasps:
@@ -231,11 +256,11 @@ def test_antipodal_grasp_sampling():
         'grasp_width': 0.1,
         'friction_coef': 0.5,
         'num_cone_faces': 8,
-        'num_samples': 4,
+        'grasp_samples_per_surface_point': 4,
         'dir_prior': 1.0,
-        'alpha_thresh': np.pi / 32,
+        'alpha_thresh': 1.0 / 32,
         'rho_thresh': 0.75, # as pct of object max moment
-        'vis_antipodal': False,
+        'grasp_theta_res': 2.0 / 10, 
         'min_num_grasps': 100,
         'alpha_inc': 1.1,
         'rho_inc': 1.1

src/grasp_selection/grasp.py
@@ -32,30 +32,6 @@ class Grasp:
         """ Converts a grasp to json """
         return None
 
-"""
-class PoseGrasp(Grasp):
-    def __init__(self, pose):
-        if not isinstance(pose, tfx.canonical.CanonicalTransform):
-            raise ValueError('Grasp pose must be a tfx transform object')
-        self.pose_ = pose
-        self.reference_frame_ = pose.from_frame
-
-    @property
-    def pose(self):
-        return self.pose_
-
-    @property
-    def reference_frame(self):
-        return self.reference_frame_
-
-    def transform(self, tf):
-        if tf.from_frame != self.pose_.to_frame:
-            logging.warning('Attempted to transform grasp between disjoint frames')
-            return False
-        self.pose_ = tf.apply(self.pose_)
-        self.reference_frame_ = slef
-   """                          
-
 class PointGrasp(Grasp):
     __metaclass__ = ABCMeta
 
@@ -67,7 +43,8 @@ class PointGrasp(Grasp):
     #NOTE: close_fingers must return success, array of contacts (one per column)
 
 class ParallelJawPtGrasp3D(PointGrasp):
-    def __init__(self, grasp_center, grasp_axis, grasp_width, jaw_width = 0, tf = stf.SimilarityTransform3D(tfx.identity_tf(from_frame = 'world'), 1.0)):
+    def __init__(self, grasp_center, grasp_axis, grasp_width, jaw_width = 0, grasp_angle = 0,
+                 tf = stf.SimilarityTransform3D(tfx.identity_tf(from_frame = 'world'), 1.0)):
         """
         Create a point grasp for parallel jaws with given center and width (relative to object)
         Params: (Note: all in meters!)
@@ -75,6 +52,9 @@ class ParallelJawPtGrasp3D(PointGrasp):
             grasp_axis - numpy 3-array for grasp direction (should be normalized)
             grasp_width - how wide the jaws open in meters
             jaw_width - the width of the jaws tangent to the axis (0 means classical point grasp)
+            grasp_angle - the angle of approach for parallel-jaw grippers (the 6th DOF for point grasps)
+                wrt the orthogonal dir to the axis in the XY plane
+            tf - the similarity tf of the grasp wrt the world
         """
         if not isinstance(grasp_center, np.ndarray) or grasp_center.shape[0] != 3:
             raise ValueError('Center must be numpy ndarray of size 3')
@@ -87,6 +67,7 @@ class ParallelJawPtGrasp3D(PointGrasp):
         self.axis_ = grasp_axis / np.linalg.norm(grasp_axis)
         self.grasp_width_ = grasp_width
         self.jaw_width_ = jaw_width
+        self.approach_angle_ = grasp_angle
         self.tf_ = tf
 
     @property
@@ -101,6 +82,9 @@ class ParallelJawPtGrasp3D(PointGrasp):
     @property
     def jaw_width(self):
         return self.jaw_width_
+    @property
+    def approach_angle(self):
+        return self.approach_angle_
     @property
     def tf(self):
         return self.tf_
@@ -144,7 +128,6 @@ class ParallelJawPtGrasp3D(PointGrasp):
 
         # find contacts
         if vis:
-            print g1_world, g2_world, self.axis_, self.grasp_width_, num_samples
             plt.cla()
             obj.sdf.scatter()
 
@@ -202,7 +185,6 @@ class ParallelJawPtGrasp3D(PointGrasp):
 
             # visualize
             if vis:
-                print pt_grid
                 ax = plt.gca(projection = '3d')
                 ax.scatter(pt_grid[0], pt_grid[1], pt_grid[2], c=u'r')
 
@@ -243,51 +225,69 @@ class ParallelJawPtGrasp3D(PointGrasp):
             pt_zc_world = obj.sdf.transform_pt_grid_to_obj(pt_zc)
         return contact_found, pt_zc_world
 
-    def pr2_gripper_poses(self, obj, theta_res = 2 * np.pi / 10, R_grasp_center = np.eye(3), t_grasp_center = PR2_GRASP_OFFSET):
+    def transform(self, tf, theta_res = 0):
         """
-        Generates a set of PR2 gripper poses in gripper_l_tool_frame relative to the given object frame
-        of reference. Since parallel-jaw grasps have 5 DOF we discretize the rotation about the orthogonal
-        direction and return all pose grasps related to that
+        Generates a set of grasps in the object frame of reference.
+        Since parallel-jaw grasps have 5 DOF we discretize the rotation about the orthogonal
+        direction and return grasps approaching along those directions
         Params:
-           obj - GraspableObject on which the grasp is generated
-           theta_res - The angle resolution for equivalent rotations
-           R_grasp_center - The rotation of the grasp control frame relative to the grasp center
-           t_grasp_center - The offset of the grasp control frame relative to the grasp center
+           tf - SimilarityTransform3D to apply to the grasp
+           theta_res - The angle resolution for equivalent rotations (defaults to zero, meaning only transform grasp)
         Returns:
-           poses - (list of tfx poses) List of the gripper poses relative to the object center
+           grasps_tf - (list of ParallelJawPtGrasp3D) list of grasps with the given transformation
         """
         # transform grasp to object basis
-        grasp_center_obj = obj.tf.apply(self.center_)
-        grasp_axis_y_obj = obj.tf.apply(self.axis_, direction=True)
-        grasp_width_obj = obj.tf.apply(self.grasp_width_)
-
-        # get x, y, and z rotational axes of PR2 gripper relative to object 
-        grasp_axis_x_obj = np.array([grasp_axis_y_obj[1], -grasp_axis_y_obj[0], 0])
-        grasp_axis_x_obj = grasp_axis_x_obj / np.linalg.norm(grasp_axis_x_obj)
-        grasp_axis_z_obj = np.cross(grasp_axis_x_obj, grasp_axis_y_obj) 
-
-        R_center_obj = np.c_[grasp_axis_x_obj.T, np.c_[grasp_axis_y_obj, grasp_axis_z_obj]]
+        grasp_center_obj = tf.apply(self.center_)
+        grasp_axis_y_obj = tf.apply(self.axis_, direction=True)
+        grasp_width_obj = tf.apply(self.grasp_width_)
 
         # store all identifications of the grasp around the rotational axes
         theta = 0
-        grasps_pr2 = []
+        grasps_tf = []
         while theta <= 2*np.pi - theta_res:
-            R_obj_rot_obj = np.array([[ np.cos(theta), 0, np.sin(theta)],
-                                      [             0, 1,             0],
-                                      [-np.sin(theta), 0, np.cos(theta)]])
-            R_center_obj_rot = R_obj_rot_obj.dot(R_center_obj)
-            R_grasp_obj = R_grasp_center.dot(R_center_obj_rot)
-
-            t_grasp_obj = grasp_center_obj + R_center_obj_rot.T.dot(t_grasp_center)
-
-            # add new grasp with given pose in object basis
-            pose_grasp_obj = tfx.transform(R_grasp_obj.T, t_grasp_obj) #TODO: add correct frame
-            grasps_pr2.append(ParallelJawPtGrasp3D(grasp_center_obj, grasp_axis_y_obj, grasp_width_obj, self.jaw_width_,
-                                                   stf.SimilarityTransform3D(pose_grasp_obj, obj.scale)))
-
+            grasps_tf.append(ParallelJawPtGrasp3D(grasp_center_obj, grasp_axis_y_obj, grasp_width_obj, self.jaw_width, theta, tf))
             theta = theta + theta_res
+        return grasps_tf
+    
+    def gripper_pose(self, R_gripper_center = np.eye(3), t_gripper_center = PR2_GRASP_OFFSET):
+        """
+        Convert a grasp to a gripper pose in SE(3) using PR2 gripper_l_too_frame as default
+        Params:
+           R_gripper_center: (numpy 3x3 array) rotation matrix from grasp basis to gripper basis
+           t_gripper_center: (numpy 3 array) translation from grasp basis to gripper basis
+        Returns:
+           pose_gripper: (tfx transform) pose of gripper in the grasp frame
+        """
+        # convert gripper orientation to rotation matrix
+        grasp_axis_y = self.axis_
+        grasp_axis_x = np.array([grasp_axis_y[1], -grasp_axis_y[0], 0])
+        grasp_axis_x = grasp_axis_x / np.linalg.norm(grasp_axis_x)
+        grasp_axis_z = np.cross(grasp_axis_x, grasp_axis_y) 
 
-        return grasps_pr2
+        R_center_ref = np.c_[grasp_axis_x, np.c_[grasp_axis_y, grasp_axis_z]]
+        pose_center_ref = tfx.transform(R_center_ref, self.center_) # pose of grasp center in its reference frame
+
+        # rotate along grasp approach angle
+        R_center_rot_center = np.array([[ np.cos(self.approach_angle_), 0, np.sin(self.approach_angle_)],
+                                        [                            0, 1,                            0],
+                                        [-np.sin(self.approach_angle_), 0, np.cos(self.approach_angle_)]])
+        pose_center_rot_center = tfx.transform(R_center_rot_center, np.zeros(3))
+        pose_gripper_center_rot = tfx.transform(R_gripper_center, t_gripper_center)
+
+        pose_gripper_ref = pose_center_ref.apply(pose_center_rot_center).apply(pose_gripper_center_rot)
+        return pose_gripper_ref
+
+        """
+        R_center_grasp_rot = R_grasp_rot_grasp.dot(R_center_grasp)
+
+        # rotation and translation in same frame as grasp
+        R_gripper = R_gripper_center.dot(R_center_grasp_rot)
+        t_gripper = self.center_ + R_center_grasp_rot.dot(t_gripper_center)
+
+        # add new grasp with given pose in object basis
+        pose_gripper = tfx.transform(R_gripper, t_gripper) #TODO: add correct frame
+        return pose_gripper
+        """
 
     @staticmethod
     def grasp_from_contact_and_axis_on_grid(obj, grasp_c1_world, grasp_axis_world, grasp_width_world, jaw_width_world = 0, vis = False):
@@ -306,9 +306,9 @@ class ParallelJawPtGrasp3D(PointGrasp):
         """
         # transform to grid basis
         grasp_axis_world = grasp_axis_world / np.linalg.norm(grasp_axis_world)
-        grasp_c1_grid = obj.sdf.transform_pt_obj_to_grid(grasp_c1_world)
         grasp_axis_grid = obj.sdf.transform_pt_obj_to_grid(grasp_axis_world, direction=True)
         grasp_width_grid = obj.sdf.transform_pt_obj_to_grid(grasp_width_world)
+        grasp_c1_grid = obj.sdf.transform_pt_obj_to_grid(grasp_c1_world) - (grasp_width_grid / 2) * grasp_axis_grid # subtract to find true point
         num_samples = int(2 * grasp_width_grid) # at least 2 samples per grid
         g2 = grasp_c1_grid + grasp_width_grid * grasp_axis_grid
 
@@ -335,7 +335,7 @@ class ParallelJawPtGrasp3D(PointGrasp):
         # create grasp
         grasp_center = ParallelJawPtGrasp3D.grasp_center_from_endpoints(c1_world, c2_world)
         grasp_axis = ParallelJawPtGrasp3D.grasp_axis_from_endpoints(c1_world, c2_world)
-        return ParallelJawPtGrasp3D(grasp_center, grasp_axis, grasp_width_world, jaw_width_world, obj.tf), c2_world # relative to object
+        return ParallelJawPtGrasp3D(grasp_center, grasp_axis, grasp_width_world, jaw_width_world, grasp_angle=0, tf=obj.tf), c2_world # relative to object
 
     def visualize(self, obj, arrow_len = 0.01, line_width = 20.0):
         """ Display point grasp as arrows on the contact points of the mesh """

src/grasp_selection/pfc.py
@@ -18,11 +18,13 @@ import similarity_tf as stf
 import tfx
 
 import discrete_adaptive_samplers as das
+import models
 import objectives
 import termination_conditions as tc
 
 import IPython
 
+# TODO: move this function somewhere interesting
 def skew(xi):
     S = np.array([[0, -xi[2], xi[1]],
                   [xi[2], 0, -xi[0]],
@@ -32,87 +34,113 @@ def skew(xi):
 class GraspableObjectGaussianPose:
     def __init__(self, obj, config):
         self.obj_ = obj
-        self.parse_config(config)
+        self._parse_config(config)
 
         self.s_rv_ = scipy.stats.norm(obj.tf.scale, self.sigma_scale_**2)
         self.t_rv_ = scipy.stats.multivariate_normal(obj.tf.translation, self.sigma_trans_**2)
         self.r_xi_rv_ = scipy.stats.multivariate_normal(np.zeros(3), self.sigma_rot_**2)
 
-    def parse_config(self, config):
+        if self.num_prealloc_samples_ > 0:
+            self._preallocate_samples()
+
+    def _parse_config(self, config):
         # NOTE: scale sigma only for now
         self.sigma_rot_ = config['sigma_rot_obj']
         self.sigma_trans_ = config['sigma_trans_obj']
         self.sigma_scale_ = config['sigma_scale_obj']
-        self.prealloc_samples = config['prealloc_obj_samples']
+        self.num_prealloc_samples_ = config['num_prealloc_obj_samples']
+
+    def _preallocate_samples(self):
+        """ Preallocate samples for faster adative sampling """
+        self.prealloc_samples_ = []
+        for i in range(self.num_prealloc_samples_):
+            self.prealloc_samples_.append(self.sample())
+
+    def sample(self, size=1):
+        """ Sample |size| random variables from the model """
+        samples = []
+        for i in range(size):
+            # sample random pose
+            xi = self.r_xi_rv_.rvs(size=1)
+            S_xi = skew(xi)
+            R = scipy.linalg.expm(S_xi).dot(self.obj_.tf.rotation)
+            s = self.s_rv_.rvs(size=1)[0]
+            t = self.t_rv_.rvs(size=1)
+            sample_tf = stf.SimilarityTransform3D(tfx.transform(R.T, t), s)
+
+            # transform object by pose
+            obj_sample = self.obj_.transform(sample_tf)
+            samples.append(obj_sample)
+
+        # not a list if only 1 sample
+        if size == 1:
+            return samples[0]
+        return samples        
 
     def rvs(self, size=1, iteration=1):
-        """ Samples |size| random variables """
-        if self.prealloc_samples:
-            todo = 1
-        else:
+        """ Samples random variables """
+        if self.num_prealloc_samples_ > 0:
             samples = []
             for i in range(size):
-                # sample random pose
-                xi = self.r_xi_rv_.rvs(size=1)
-                S_xi = skew(xi)
-                R = scipy.linalg.expm(S_xi).dot(self.obj_.tf.rotation)
-                s = self.s_rv_.rvs(size=1)[0]
-                t = self.t_rv_.rvs(size=1)
-
-                sample_tf = stf.SimilarityTransform3D(tfx.transform(R.T, t), s)
-
-                # transform object by pose
-                obj_sample = self.obj_.transform(sample_tf)
-
-                """
-                print 'R', R
-                print 'T', t
-                print 'S', s
-
-                obj_sample.sdf.scatter()
-                plt.show()
-                """
-                samples.append(obj_sample)
-
+                samples.append(self.prealloc_samples_[(iteration + i) % self.num_prealloc_samples_])
             if size == 1:
                 return samples[0]
-            return samples
+            return samples        
+        # generate a new sample
+        return self.sample(size=size)
 
 class ParallelJawGraspGaussian:
     def __init__(self, grasp, config):
         self.grasp_ = grasp
-        self.parse_config(config)
+        self._parse_config(config)
 
         self.t_rv_ = scipy.stats.multivariate_normal(grasp.center, self.sigma_trans_**2)
         self.r_xi_rv_ = scipy.stats.multivariate_normal(np.zeros(3), self.sigma_rot_**2)
 
-    def parse_config(self, config):
+        if self.num_prealloc_samples_ > 0:
+            self._preallocate_samples()
+
+    def _parse_config(self, config):
         # NOTE: scale sigma only for now
         self.sigma_rot_ = config['sigma_rot_grasp']
         self.sigma_trans_ = config['sigma_trans_grasp']
-        self.prealloc_samples = config['prealloc_grasp_samples']
+        self.num_prealloc_samples_ = config['num_prealloc_grasp_samples']
+
+    def _preallocate_samples(self):
+        """ Preallocate samples for faster adative sampling """
+        self.prealloc_samples_ = []
+        for i in range(self.num_prealloc_samples_):
+            self.prealloc_samples_.append(self.sample())
+
+    def sample(self, size=1):
+        samples = []
+        for i in range(size):
+            # sample random pose
+            xi = self.r_xi_rv_.rvs(size=1)
+            S_xi = skew(xi)
+            v = scipy.linalg.expm(S_xi).dot(self.grasp_.axis)
+            t = self.t_rv_.rvs(size=1)
+
+            # transform object by pose
+            grasp_sample = copy.copy(self.grasp_) #gr.ParallelJawPtGrasp3D(t, v, self.grasp_.grasp_width)
+
+            samples.append(grasp_sample)
+
+        if size == 1:
+            return samples[0]
+        return samples
 
     def rvs(self, size=1, iteration=1):
         """ Samples |size| random variables """
-        if self.prealloc_samples:
-            todo = 1
-        else:
+        if self.num_prealloc_samples_ > 0:
             samples = []
             for i in range(size):
-                # sample random pose
-                xi = self.r_xi_rv_.rvs(size=1)
-                S_xi = skew(xi)
-                v = scipy.linalg.expm(S_xi).dot(self.grasp_.axis)
-                t = self.t_rv_.rvs(size=1)
-
-                # transform object by pose
-                grasp_sample = copy.copy(self.grasp_) #gr.ParallelJawPtGrasp3D(t, v, self.grasp_.grasp_width)
-
-                samples.append(grasp_sample)
-
+                samples.append(self.prealloc_samples_[(iteration + i) % self.num_prealloc_samples_])
             if size == 1:
                 return samples[0]
-            return samples
+            return samples        
+        # generate a new sample
+        return self.sample(size=size)
 
 class ForceClosureRV:
     """ RV class for grasps in force closure on an object """
@@ -129,10 +157,10 @@ class ForceClosureRV:
 
     def sample_success(self):
         # sample grasp
-        grasp_sample = self.grasp_rv_.rvs(size=1)
+        grasp_sample = self.grasp_rv_.rvs(size=1, iteration=self.sample_count_)
 
         # sample object
-        obj_sample = self.obj_rv_.rvs(size=1)
+        obj_sample = self.obj_rv_.rvs(size=1, iteration=self.sample_count_)
 
         # sample friction cone
         friction_coef_sample = self.friction_coef_rv_.rvs(size=1)
@@ -144,19 +172,28 @@ class ForceClosureRV:
         plt.show()
         IPython.embed()
         """
-
         # compute force closure
         fc = pgq.PointGraspMetrics3D.grasp_quality(grasp_sample, obj_sample, "force_closure", friction_coef = friction_coef_sample,
                                                    num_cone_faces = self.num_cone_faces_, soft_fingers = True)
         self.sample_count_ = self.sample_count_ + 1
         return fc
 
+def plot_value_vs_time_beta_bernoulli(result, candidate_true_p, true_max=None, color='blue'):
+    """ Plots the number of samples for each value in for a discrete adaptive sampler"""
+    best_values = [candidate_true_p[m.best_pred_ind] for m in result.models]
+    plt.plot(result.iters, best_values, color=color, linewidth=2)
+    if true_max is not None: # also plot best possible
+        plt.plot(result.iters, true_max*np.ones(len(result.iters)), color='green', linewidth=2)
+    plt.xlabel('Iteration')
+    plt.ylabel('Probability of Success')
+
 def test_antipodal_grasp_thompson():
     np.random.seed(100)
 
-    h = plt.figure()
-    ax = h.add_subplot(111, projection = '3d')
+#    h = plt.figure()
+#    ax = h.add_subplot(111, projection = '3d')
 
+    # load object
     sdf_3d_file_name = 'data/test/sdf/Co_clean.sdf'
     sf = sdf_file.SdfFile(sdf_3d_file_name)
     sdf_3d = sf.read()
@@ -171,7 +208,7 @@ def test_antipodal_grasp_thompson():
         'grasp_width': 0.1,
         'friction_coef': 0.5,
         'num_cone_faces': 8,
-        'num_samples': 4,
+        'grasp_samples_per_surface_point': 4,
         'dir_prior': 1.0,
         'alpha_thresh': np.pi / 32,
         'rho_thresh': 0.75, # as pct of object max moment
@@ -185,8 +222,8 @@ def test_antipodal_grasp_thompson():
         'sigma_trans_obj': 0.001,
         'sigma_rot_obj': 0.1,
         'sigma_scale_obj': 0.1,
-        'prealloc_obj_samples': False,
-        'prealloc_grasp_samples': False
+        'num_prealloc_obj_samples': 100,
+        'num_prealloc_grasp_samples': 0
     }
     sampler = ags.AntipodalGraspSampler(config)
 
@@ -206,14 +243,23 @@ def test_antipodal_grasp_thompson():
         grasp_rv = ParallelJawGraspGaussian(grasp, config)
         candidates.append(ForceClosureRV(grasp_rv, graspable_rv, f_rv, config))
 
+    IPython.embed()
     objective = objectives.RandomBinaryObjective()
 
+    # run bandits
     ua = das.UniformAllocationMean(objective, candidates)
-    ua_result = ua.solve(termination_condition = tc.MaxIterTerminationCondition(10000), snapshot_rate = 1000)
+    ua_result = ua.solve(termination_condition = tc.MaxIterTerminationCondition(10000), snapshot_rate = 100)
 
     ts = das.ThompsonSampling(objective, candidates)
     ts_result = ts.solve(termination_condition = tc.MaxIterTerminationCondition(1000), snapshot_rate = 100)
 
+    true_means = models.BetaBernoulliModel.beta_mean(ua_result.models[-1].alphas, ua_result.models[-1].betas)
+
+    # plot results
+    plt.figure()
+    plot_value_vs_time_beta_bernoulli(ua_result, true_means, color='red')
+    plot_value_vs_time_beta_bernoulli(ts_result, true_means, color='blue')
+    plt.show()
     IPython.embed()
 
     das.plot_num_pulls_beta_bernoulli(ua_result)

src/grasp_selection/pr2_grasp_checker.py
@@ -1,10 +1,12 @@
 """
 +X is front, +Y is left, +Z is up
 """
+import copy
 import os
 import sys
 import time
 
+import matplotlib.pyplot as plt
 import mayavi.mlab as mv
 import numpy as np
 import openravepy as rave
@@ -12,6 +14,11 @@ import IK as ik
 from pymongo import MongoClient
 
 from message_wrappers import GraspWrapper
+
+import grasp as g
+import graspable_object
+import obj_file
+import sdf_file
 
 import IPython
 
@@ -53,7 +60,9 @@ class PR2GraspChecker(object):
         self.T_gripper_world = self.robot.GetManipulator("leftarm_torso").GetTransform()
 
         # set transform between rviz and openrave
-        R_fix = np.array([[0, 0, -1], [0, 1, 0], [1, 0, 0]])
+        R_fix = np.array([[0, 0, -1],
+                          [0, 1, 0],
+                          [1, 0, 0]])
         T_fix = np.eye(4)
         T_fix[:3,:3] = R_fix
         T_fix[1,3] = 0
@@ -133,7 +142,7 @@ class PR2GraspChecker(object):
                     self.taskprob.ReleaseFingers() # open fingets
                     self.robot.WaitForController(0) # wait
 
-    def prune_grasps_in_collision(self, object_grasps, vis = True, closed_thresh = 0.05, auto_step = True, close_fingers = False):
+    def prune_grasps_in_collision(self, object_grasps, vis = True, closed_thresh = 0.05, auto_step = False, close_fingers = False):
         """ Remove all grasps from the object grasps list that are in collision with the given object """
         if vis and self.env.GetViewer() is None:
             self.env.SetViewer('qtcoin')
@@ -142,9 +151,9 @@ class PR2GraspChecker(object):
         i = 0
 
         for grasp in object_grasps:
+            pose = grasp.gripper_pose()
             if vis:
-                T_gripper_obj, T_robot_world = self.move_to_pregrasp(grasp.tf.pose)
-
+                T_gripper_obj, T_robot_world = self.move_to_pregrasp(pose)
             """
             link = self.robot.GetLink('l_gripper_palm_link')
             link_geoms = link.GetGeometries()
@@ -185,7 +194,69 @@ class PR2GraspChecker(object):
 
         return object_grasps_keep
 
+def test_grasp_collisions():
+    np.random.seed(100)
+
+    h = plt.figure()
+    ax = h.add_subplot(111, projection = '3d')
+
+    sdf_3d_file_name = 'data/test/sdf/Co_clean.sdf'
+    sf = sdf_file.SdfFile(sdf_3d_file_name)
+    sdf_3d = sf.read()
+
+    mesh_name = 'data/test/meshes/Co_clean.obj'
+    of = obj_file.ObjFile(mesh_name)
+    m = of.read()
+
+    graspable = graspable_object.GraspableObject3D(sdf_3d, mesh=m)
+
+    rave.raveSetDebugLevel(rave.DebugLevel.Error)
+    e = rave.Environment()
+    e.Load(PR2_MODEL_FILE)
+    e.SetViewer("qtcoin")
+    r = e.GetRobots()[0]
+    grasp_checker = PR2GraspChecker(e, r, mesh_name)
+
+    """
+    z = np.linspace(-0.1, 0.1, 50)
+    T = grasp_checker.object.GetTransform()
+    for i in range(z.shape[0]):
+        print z
+        T_tf = copy.copy(T)
+        T_tf[2,3] = z[i]
+        grasp_checker.object.SetTransform(T_tf)
+        time.sleep(0.1)
+
+    for i in range(z.shape[0]):
+        print z
+        T_tf = copy.copy(T)
+        T_tf[1,3] = z[i]
+        grasp_checker.object.SetTransform(T_tf)
+        time.sleep(0.1)
+
+    for i in range(z.shape[0]):
+        print z
+        T_tf = copy.copy(T)
+        T_tf[0,3] = z[i]
+        grasp_checker.object.SetTransform(T_tf)
+        time.sleep(0.1)
+    """
+
+    center = np.array([-0.01837182,  0.01218656,  0.03844461])
+    # np.array([-0.00297651,  0.00677959,  0.03165122])
+    axis = np.array([-0.94212793,  0.31667146, -0.11006428]) 
+    #np.array([1, 1, 0])
+    axis = axis / np.linalg.norm(axis)
+    grasp = g.ParallelJawPtGrasp3D(center, axis, 0.1)
+
+    grasp.close_fingers(graspable, vis = True)
+    grasp_checker.prune_grasps_in_collision([grasp], vis = True)
+
+    IPython.embed()
+
 if __name__ == "__main__":
+    test_grasp_collisions()
+    """
     # get sys input
     argc = len(sys.argv)
     if argc < 2:
@@ -212,3 +283,4 @@ if __name__ == "__main__":
                                               "{}.json".format(object_name + '_coll_free'))
     GraspWrapper.grasps_to_file(object_grasps_keep, object_grasps_out_filename)
 
+    """

src/grasp_selection/sdf.py
@@ -216,7 +216,7 @@ class Sdf3D(Sdf):
 
         # set up surface params
         self.surface_thresh_ = self.resolution_ * np.sqrt(2) / 2 # resolution is max dist from surface when surf is orthogonal to diagonal grid cells
-        self.center_ = np.array([self.dims_[0] / 2, self.dims_[1] / 2, self.dims_[2] / 2])
+        self.center_ = np.array([(self.dims_[0]-1) / 2, (self.dims_[1]-1) / 2, (self.dims_[2]-1) / 2])
 
         # set up tf
         self.tf_ = tf