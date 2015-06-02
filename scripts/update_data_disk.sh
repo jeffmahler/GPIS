cdf0a248eb7ff2d5f6b7ef0324345121389fe810
Brian Hou <brian.hou@live.com>
2015-06-02 14:41:53 -0700

    Add script for updating data disk.

---
+++ scripts/update_data_disk.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+#
+# Script for syncing Google Cloud Storage bucket with GCE disk.
+
+BUCKET=gs://dex-net/
+DEVICE=/dev/sdb
+TARGET=~/data
+
+# Create data directory and mount disk
+mkdir -p $TARGET
+sudo /usr/share/google/safe_format_and_mount -m "mkfs.ext4 -F" $DEVICE $TARGET
+
+# Sync bucket -- https://cloud.google.com/storage/docs/gsutil/commands/rsync
+sudo gsutil -m rsync -r $BUCKET $TARGET
