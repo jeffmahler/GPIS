c1733432bbf7c7f226fef10c957a8c54934044a4
Brian Hou <brian.hou@live.com>
2015-06-05 10:37:15 -0700

    Fix minor bug and document upload_results.

--- scripts/upload_results.py
+++ scripts/upload_results.py
@@ -2,8 +2,8 @@
 Command-line utility to upload GCE results to database.
 
 $ python upload_results.py cm_out gs://dex-net/ \
-  gs://dex-net-cm/experiment-pqfcudqamp-0.tar.gz \
-  gs://dex-net-cm/experiment-pqfcudqamp-1.tar.gz
+  gs://dex-net-cm/experiment-a-*.tar.gz \
+  gs://dex-net-cm/experiment-b-0.tar.gz
 """
 
 import argparse
@@ -30,23 +30,31 @@ def main():
         os.mkdir(TMP_DIR)
     except os.error:
         pass
-    for result in results:
+
+    # Download results from GCS
+    for result in args.results:
         name = os.path.basename(result)
         command = ['gsutil', 'cp', result, TMP_DIR]
         run(command)
-        with tarfile.open(os.path.join(TMP_DIR, name)) as tar:
-            tar.extractall(TMP_DIR)
 
+    # Extract all .tar.gz files
+    for name in os.listdir(TMP_DIR):
+        if name.endswith('.tar.gz'):
+            with tarfile.open(os.path.join(TMP_DIR, name)) as tar:
+                tar.extractall(TMP_DIR)
+
+    # Find all tmp_dir/result_dir/dataset directories
     to_upload = []
     result_root = os.path.join(TMP_DIR, args.result_dir)
     for directory, directories, files in os.walk(result_root):
         to_upload.append(directory)
     to_upload.pop(0)
 
+    # Upload files to GCS database
     for directory in to_upload:
-        command = ['gsutil', '-m', 'cp', '-r', directory, database]
-        if no_clobber:
-            command = ['gsutil', '-m', 'cp', '-n', '-r', directory, database]
+        command = ['gsutil', '-m', 'cp', '-r', directory, args.database]
+        if args.no_clobber:
+            command = ['gsutil', '-m', 'cp', '-n', '-r', directory, args.database]
         run(command)
 
 if __name__ == '__main__':
