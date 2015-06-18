9175b129e501cfb20d7935f89d5ff00d2b7f543b
Nikhil <ennsharma@berkeley.edu>
2015-06-18 11:16:05 -0700

    Debugging in progress.

--- src/grasp_selection/stable_poses.py
+++ src/grasp_selection/stable_poses.py
@@ -2,13 +2,15 @@
 Computes the statistical distribution of stable poses for a polyhedron
 Author: Nikhil Sharma
 """
-
 import math
 import sys
 import numpy as np
+import Queue
 
 import mesh
 import obj_file
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 # A function for computing the statistical distribution of stable poses of a polyhedron.
 def compute_stable_poses(mesh):
@@ -19,12 +21,10 @@ def compute_stable_poses(mesh):
     mesh -- a 3D Mesh object
     """
     convex_hull, cm = mesh.convex_hull(), mesh.vertex_mean_
-    print(mesh.vertices()
-        )
-    print(cm)
 
     # mapping each edge in the convex hull to the two faces it borders
     triangles, vertices, edge_to_faces, triangle_to_vertex = convex_hull.triangles(), convex_hull.vertices(), {}, {}
+
     for triangle in triangles:
         triangle_vertices = [vertices[i] for i in triangle]
         e1, e2, e3 = Segment(triangle_vertices[0], triangle_vertices[1]), Segment(triangle_vertices[0], triangle_vertices[2]), Segment(triangle_vertices[1], triangle_vertices[2])
@@ -72,8 +72,16 @@ def compute_stable_poses(mesh):
         # update list of top vertices, add edges between vertices as needed
         if not proj_cm_in_triangle:
             s1, s2, s3 = Segment(triangle_vertices[0], triangle_vertices[1]), Segment(triangle_vertices[0], triangle_vertices[2]), Segment(triangle_vertices[1], triangle_vertices[2])
-            closest_edge = closest_segment(proj_cm, [s1, s2, s3])
-
+            closest_edges, common_endpoint = closest_segment(proj_cm, [s1, s2, s3])
+            if len(closest_edges) > 1:
+                index = triangle_vertices.index(common_endpoint)
+                if index == len(triangle_vertices) - 1:
+                    index = 0
+                else:
+                    index = index + 1
+                closest_edge = Segment(common_endpoint, triangle_vertices[index])
+            else:
+                closest_edge = closest_edges[0]
             p_1, p_2 = tuple(sorted(closest_edge.endpoint_1)), tuple(sorted(closest_edge.endpoint_2))
             k = (p_1, p_2) if p_1[0] > p_2[0] else (p_2, p_1)
             for face in edge_to_faces[k]:
@@ -83,22 +91,11 @@ def compute_stable_poses(mesh):
             predecessor.add_edge(successor)
 
     # computes dictionary mapping faces to probabilities
-    probabilities = {}
+    top_vertices = []
     for vertex in triangle_to_vertex.values():
         if not vertex.has_parent:
-            probabilities.update(propogate_probabilities(vertex))
-
-    # probability normalization
-    prob_sum = 0
-    for k, v in probabilities.items():
-        if math.isnan(v):
-            del probabilities[k]
-        else:
-            prob_sum += v
-    norm_factor = 1.0/prob_sum
-
-    for k in probabilities.keys():
-        probabilities[k] *= norm_factor
+            top_vertices.append(vertex)
+    probabilities = propagate_probabilities(top_vertices)
 
     return probabilities
 
@@ -110,8 +107,8 @@ def compute_projected_area(vertices, cm):
     triangle_vertices -- list of three 3-element arrays; each array is a vertex coordinate of the triangle
     cm -- 3-element array representing the center of mass of the mesh being handled
     """
-    angles, projected_vertices = [], [np.subtract(vertex, cm) / np.linalg.norm(vertex) for vertex in vertices]
-    
+    angles, projected_vertices = [], [np.subtract(vertex, cm) / np.linalg.norm(np.subtract(vertex, cm)) for vertex in vertices]
+
     a = math.acos(np.dot(projected_vertices[0], projected_vertices[1]) / (np.linalg.norm(projected_vertices[0]) * np.linalg.norm(projected_vertices[1])))
     b = math.acos(np.dot(projected_vertices[0], projected_vertices[2]) / (np.linalg.norm(projected_vertices[0]) * np.linalg.norm(projected_vertices[2])))
     c = math.acos(np.dot(projected_vertices[1], projected_vertices[2]) / (np.linalg.norm(projected_vertices[1]) * np.linalg.norm(projected_vertices[2])))
@@ -120,7 +117,7 @@ def compute_projected_area(vertices, cm):
     return 4 * math.atan(math.sqrt(math.tan(s/2)*math.tan((s-a)/2)*math.tan((s-b)/2)*math.tan((s-c)/2)))
     
 
-def propogate_probabilities(vertex):
+def propagate_probabilities(top_vertices):
     """
     Traverses graph of faces with depth first search, setting all non-sink
     probabilities to 0 and all sink probabilities to the sum of all parent
@@ -130,16 +127,26 @@ def propogate_probabilities(vertex):
     prob_sum -- current probability sum
     prob_mapping -- mapping of sink vertices to their propogated probabilities
     """
-    prob_sum, prob_mapping = vertex.probability, {}
-    while not vertex.is_sink:
-        vertex = vertex.child
-        if not vertex.marked:
-            vertex.marked = True
-            prob_sum += vertex.probability
-    if tuple(vertex.face) in prob_mapping.keys():
-        prob_mapping[tuple(vertex.face)] += prob_sum
-    else:
-        prob_mapping[tuple(vertex.face)] = prob_sum
+    q, prob_mapping = Queue.Queue(), {}
+    for vertex in top_vertices:
+        q.put(vertex)
+
+    while not q.empty():
+        curr_vertex = q.get()
+        if curr_vertex.marked:
+            prob_mapping[tuple(curr_vertex.face)] = curr_vertex.probability
+            continue
+        if curr_vertex.is_sink:
+            prob_mapping[tuple(curr_vertex.face)] = curr_vertex.probability
+        else:
+            num_children = len(curr_vertex.children)
+            curr_vertex.probability /= num_children
+            for child in curr_vertex.children:
+                if curr_vertex in child.children:
+                    print(curr_vertex.face)
+                child.probability += curr_vertex.probability
+                q.put(child)
+        curr_vertex.marked = True;
     return prob_mapping
 
 
@@ -150,7 +157,7 @@ def closest_segment(point, line_segments):
     point -- A 3-element array-like data structure containing x,y,z coordinates
     line_segments -- An array of Segments
     """
-    min_dist = sys.maxint
+    min_dist, min_segs, distances, segments, common_endpoint = sys.maxint, [], [], [], None
     for segment in line_segments:
         proj_point = project(point, segment)
         if on_line_segment(proj_point, segment):
@@ -158,10 +165,23 @@ def closest_segment(point, line_segments):
         else:
             min_endpoint_distance = min(distance_between(proj_point, segment.endpoint_1), distance_between(proj_point, segment.endpoint_2))
             dist = math.sqrt((distance_between(point, proj_point))**2 + (min_endpoint_distance)**2)
-
+        distances.append(dist)
+        segments.append(segment)
         if dist < min_dist:
-            min_dist, min_seg = dist, segment
-    return min_seg
+            min_dist = dist
+
+    for i in range(len(distances)):
+        if min_dist + 0.000001 >= distances[i]:
+            min_segs.append(segments[i])
+    if len(min_segs) > 1:
+        for i in range(len(min_segs)):
+            seg_1 = min_segs[i]
+            for endpoint in [seg_1.endpoint_1, seg_1.endpoint_2]:
+                for seg_2 in min_segs[i+1:]:
+                    if endpoint in [seg_2.endpoint_1, seg_2.endpoint_2]:
+                        common_endpoint = endpoint
+                        break
+    return min_segs, common_endpoint
 
 
 def distance_between(point_1, point_2):
@@ -198,9 +218,11 @@ def project(point, segment):
     point -- a 3-element array containing x,y,z coordinates
     segment -- a Segment object
     """
-    vector_segment = np.subtract(segment.endpoint_1, segment.endpoint_2)
-    scale_factor = (np.dot(vector_segment, np.subtract(point, segment.endpoint_1)) / np.dot(vector_segment, vector_segment))
-    return np.add(segment.endpoint_1, scale_factor*vector_segment)
+    x = np.linalg.norm(np.subtract(point, segment.endpoint_1))
+    y = np.linalg.norm(np.subtract(point, segment.endpoint_2))
+    z = np.linalg.norm(np.subtract(segment.endpoint_2, segment.endpoint_1))
+    scale_factor = 0.5*(z - ((x**2 - y**2) / z))
+    return np.add(segment.endpoint_2, scale_factor * (np.subtract(segment.endpoint_1, segment.endpoint_2) / np.linalg.norm(np.subtract(segment.endpoint_1, segment.endpoint_2))))
 
 
 class Segment:
@@ -226,7 +248,7 @@ class Vertex:
     """
     marked = False
 
-    def __init__(self, probability, face, child=None):
+    def __init__(self, probability, face, chilren=[]):
         """
         Create a Vertex with given probability and children
         
@@ -234,9 +256,9 @@ class Vertex:
         children -- array of vertices pointed at by this vertex
         """
         self.probability = probability
-        self.child = child
+        self.children = []
         self.face = face
-        self.is_sink = True if not self.child else False
+        self.is_sink = True if not self.children else False
         self.has_parent = False
         self.num_parents = 0
 
@@ -247,7 +269,7 @@ class Vertex:
         child -- Vertex object
         """
         self.is_sink = False
-        self.child = child
+        self.children.append(child)
         child.has_parent = True
         child.num_parents += 1
 
@@ -255,11 +277,12 @@ class Vertex:
 ##############
 #TESTING AREA#
 ##############
-# filename = "/Users/Nikhil/Desktop/UC Berkeley /GPIS/data/test/meshes/Co_clean.obj"
-filename = "/Users/Nikhil/Desktop/pyramid.obj"
+filename = "/Users/Nikhil/Desktop/UC Berkeley /GPIS/data/test/meshes/Co_clean.obj"
+filename = "/Users/Nikhil/Desktop/pepper_orig.obj"
 ob = obj_file.ObjFile(filename)
 mesh = ob.read()
 mesh.remove_unreferenced_vertices()
 prob_mapping = compute_stable_poses(mesh)
 print(prob_mapping)
 print(sum([val for val in prob_mapping.values()]))
+
