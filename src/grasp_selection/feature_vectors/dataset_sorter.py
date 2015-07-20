f97c805861d6e78c5807e74514c54527db4a6450
Mel Roderick <melrose_roderick@brown.edu>
2015-07-20 14:24:23 -0700

    Fixed bugs for running CNN training trials

--- src/grasp_selection/feature_vectors/dataset_sorter.py
+++ src/grasp_selection/feature_vectors/dataset_sorter.py
@@ -3,12 +3,12 @@ import mesh_database
 from feature_database import FeatureDatabase
 
 class DatasetSorter:
-	def __init__(self, feature_database):
-		object_keys = feature_database.mesh_database().object_keys().keys()
+	def __init__(self, feature_db):
+		object_keys = feature_db.mesh_database().object_keys()
 		random.shuffle(object_keys)
 		length = len(object_keys)
-		self.train_object_keys_ = object_keys[:int(length*portion_training)]
-		self.test_object_keys_ = object_keys[int(length*portion_training):length]
+		self.train_object_keys_ = object_keys[:int(length*feature_db.portion_training())]
+		self.test_object_keys_ = object_keys[int(length*feature_db.portion_training()):length]
 
 	def train_object_keys(self):
 		return self.train_object_keys_
