2e737fdd50b3b3c6476bec3127cf7a1ae9477508
Brian Hou <brian.hou@live.com>
2015-07-27 14:19:24 -0700

    Add back logging on every iteration.

--- src/grasp_selection/discrete_adaptive_samplers.py
+++ src/grasp_selection/discrete_adaptive_samplers.py
@@ -99,7 +99,7 @@ class DiscreteAdaptiveSampler(solvers.DiscreteSamplingSolver):
 
             # snapshot the model and whatnot
             if (k % snapshot_rate) == 0:
-#                logging.info('Iteration %d' %(k))
+                logging.info('Iteration %d' %(k))
 
                 # log time and stuff
                 checkpt = time.clock()
