929535c4a6961fba88b7899db3df1925c74b27a1
Mel Roderick <melrose_roderick@brown.edu>
2015-07-29 16:47:50 -0700

    Updated correlated_bandits_priors for running trials. Also added plot_kernels_for_key for plotting the kernels of the grasps between objects

--- src/grasp_selection/prior_computation_engine.py
+++ src/grasp_selection/prior_computation_engine.py
@@ -5,9 +5,10 @@ from feature_database import FeatureDatabase
 import kernels
 import database
 import feature_functions as ff
+import time
 
 class PriorComputationEngine:
-	def __init__(self, db, grasp_kernel, config, feature_db=None):
+	def __init__(self, db, config, feature_db=None):
 		if feature_db == None:
 			self.feature_db = FeatureDatabase()
 		else:
@@ -18,16 +19,19 @@ class PriorComputationEngine:
 		self.neighbor_kernel = kernels.SquaredExponentialKernel(
 			sigma=1.0, l=config['prior_neighbor_kernel_l'])
 		self.neighbor_distance = config['prior_neighbor_distance']
+		self.num_neighbors = config['prior_num_neighbors']
 		self.config = config
 
 	def compute_priors(self, key, candidates):
 		nf = self.feature_db.nearest_features()
 		feature_vector = nf.project_feature_vector(self.feature_db.feature_vectors()[key])
-		neighbor_vector_dict = nf.within_distance(feature_vector, dist=self.neighbor_distance)
+		neighbor_vector_dict = nf.k_nearest(feature_vector, k=self.num_neighbors+1)#nf.within_distance(feature_vector, dist=self.neighbor_distance)
+		# return neighbor_vector_dict
 		print 'Found %d neighbors!' % (len(neighbor_vector_dict))
 		print 'Loading features...'
 		neighbor_features_for_key = {}
 		neighbor_grasps_for_key = {}
+
 		for neighbor_key in neighbor_vector_dict:
 			if neighbor_key == key:
 				continue
@@ -38,6 +42,7 @@ class PriorComputationEngine:
 		print 'Finished loading features'
 
 		print 'Creating priors...'
+		prior_compute_start = time.clock()
 		alpha_priors = []
 		beta_priors = []
 		for candidate in candidates:
@@ -53,11 +58,30 @@ class PriorComputationEngine:
 					beta += object_distance * grasp_distance * neighbor_grasp.failures
 			alpha_priors.append(alpha)
 			beta_priors.append(beta)
-		print 'Finished creating priors'
+		prior_compute_end = time.clock()
+		print 'Finished creating priors. TIME: %.2f' % (prior_compute_end - prior_compute_start)
 
 		return alpha_priors, beta_priors
 
-	def compute_priors_with_neighbor(self, key, candidates, neighbor_key):
+	def compute_grasp_kernels(self, key, candidates):
+		nf = self.feature_db.nearest_features()
+		feature_vector = nf.project_feature_vector(self.feature_db.feature_vectors()[key])
+		neighbor_vector_dict = nf.k_nearest(feature_vector, k=self.num_neighbors+1)
+		neighbor_keys = neighbor_vector_dict.keys()
+		all_neighbor_pfc_diffs = []
+		all_neighbor_kernels = []
+		all_distances = []
+		object_indices = range(0, 28)
+		for neighbor_key in neighbor_keys:
+			if neighbor_key == key:
+				continue
+			neighbor_pfc_diffs, neighbor_kernels, object_distance = self.kernel_info_from_neighbor(key, candidates, neighbor_key)
+			all_neighbor_pfc_diffs.append(neighbor_pfc_diffs)
+			all_neighbor_kernels.append(neighbor_kernels)
+			all_distances.append(object_distance)
+		return neighbor_keys, all_neighbor_kernels, all_neighbor_pfc_diffs, all_distances
+
+	def kernel_info_from_neighbor(self, key, candidates, neighbor_key):
 		nf = self.feature_db.nearest_features()
 		feature_vector = nf.project_feature_vector(self.feature_db.feature_vectors()[key])
 		neighbor_vector = nf.project_feature_vector(self.feature_db.feature_vectors()[neighbor_key])
@@ -69,6 +93,7 @@ class PriorComputationEngine:
 		print 'Finished loading features'
 
 		print 'Creating priors...'
+		prior_compute_start = time.clock()
 		alpha_priors = []
 		beta_priors = []
 		neighbor_pfc_diffs = []
@@ -82,16 +107,14 @@ class PriorComputationEngine:
 				grasps = neighbor_grasps_for_key[neighbor_key]
 				for neighbor_grasp, features in zip(grasps, all_features):
 					grasp_distance = self.grasp_kernel(candidate.features, features.phi)
-					alpha += object_distance * grasp_distance * neighbor_grasp.successes
-					beta += object_distance * grasp_distance * neighbor_grasp.failures
-
 					neighbor_pfc_diffs.append(abs(candidate.grasp.quality - neighbor_grasp.quality))
 					neighbor_kernels.append(grasp_distance)
 			alpha_priors.append(alpha)
 			beta_priors.append(beta)
-		print 'Finished creating priors'
+		prior_compute_end = time.clock()
+		print 'Finished creating priors. TIME: %.2f' % (prior_compute_end - prior_compute_start)
 
-		return alpha_priors, beta_priors, neighbor_pfc_diffs, neighbor_kernels
+		return neighbor_pfc_diffs, neighbor_kernels, object_distance
 
 	def _load_grasps_and_features_for_key(self, key):
 		obj = self.db[key]
