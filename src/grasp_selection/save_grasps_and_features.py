31de19d7190eb70449eee38cbc7f4ad08a87226d
Brian Hou <brian.hou@live.com>
2015-07-28 17:26:34 -0700

    Use shorter directions in save_grasps command.

--- src/grasp_selection/save_grasps_and_features.py
+++ src/grasp_selection/save_grasps_and_features.py
@@ -15,11 +15,7 @@ if __name__ == '__main__':
 
     config = ec.ExperimentConfig(args.config)
     target_dir = os.path.join(config['database_dir'], config['dataset'])
-
-    for root, dirs, files in os.walk(args.result_dir):
-        if len(files) == len(dirs) == 1:
-            # in directory with relevant files
-            file, dir = files[0], dirs[0]
-            grasp_file = os.path.join(root, file)
-            feature_dir = os.path.join(root, dir)
-            print 'sudo cp -r %s %s %s' %(grasp_file, feature_dir, target_dir)
+    json = os.path.join(args.result_dir, '*/cm_out/test_data/*.json')
+    features = os.path.join(args.result_dir, '*/cm_out/test_data/features')
+    logging.info('Run this command yourself:')
+    print 'sudo cp -r %s %s %s' %(json, features, target_dir)
