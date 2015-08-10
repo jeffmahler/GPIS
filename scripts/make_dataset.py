1c6ef7247703708321bbfd25dfb3642ef380c017
Brian Hou <brian.hou@live.com>
2015-08-10 01:07:58 -0700

    Fix dataset script.

--- scripts/make_dataset.py
+++ scripts/make_dataset.py
@@ -23,7 +23,7 @@ new_feature_path = os.path.join(new_dataset_path, 'features')
 
 with open(args.dataset) as f:
     os.mkdir(new_dataset_path)
-    os.mkdir(new_feature_path)
+    # os.mkdir(new_feature_path)
     for line in f:
         tokens = line.split()
         orig_dataset = tokens[0]
@@ -34,7 +34,8 @@ with open(args.dataset) as f:
         old_feature_path = os.path.join(old_dataset_path, 'features')
 
         # create symlinks
-        for ext in ('.obj', '.sdf', '.json'):
+        # for ext in ('.obj', '.sdf', '.json'):
+        for ext in ('.obj', '.sdf'):
             fname = key + ext
             src = os.path.join(old_dataset_path, fname)
             link_name = '%s_%s' %(orig_dataset, fname)
@@ -42,16 +43,17 @@ with open(args.dataset) as f:
             os.symlink(src, dst)
 
         # symlink features
-        src = os.path.join(old_feature_path, key + '.json')
-        dst = os.path.join(new_feature_path, key + '.json')
-        os.symlink(src, dst)
+        # src = os.path.join(old_feature_path, key + '.json')
+        # dst = os.path.join(new_feature_path, key + '.json')
+        # os.symlink(src, dst)
 
-        for src in glob.iglob(os.path.join(old_feature_path, key + '*')):
-            dst = os.path.join(new_feature_path, os.path.basename(src))
-            os.symlink(src, dst)
+        # for src in glob.iglob(os.path.join(old_feature_path, key + '*')):
+        #     dst = os.path.join(new_feature_path, os.path.basename(src))
+        #     os.symlink(src, dst)
 
         # add "origdataset_key category" to index_db_lines
-        index_db_line = '%s %s\n' %(link_name, category) if category else link_name
+        new_key = '%s_%s' %(orig_dataset, key)
+        index_db_line = '%s %s\n' %(new_key, category) if category else new_key
         index_db_lines.append(index_db_line)
 
 new_index = os.path.join(DATABASE_PATH, args.dataset, 'index.db')
