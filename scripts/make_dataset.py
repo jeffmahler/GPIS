be27ac0fdb016a91f6712785a2e616597674d85c
Brian Hou <brian.hou@live.com>
2015-08-12 12:39:14 -0700

    Modify make_dataset script to make real copies on terastation.

--- scripts/make_dataset.py
+++ scripts/make_dataset.py
@@ -10,15 +10,22 @@ Author: Brian Hou
 import argparse
 import glob
 import os
-
-DATABASE_PATH = '/home/brian/data'
+import shutil
 
 parser = argparse.ArgumentParser()
 parser.add_argument('dataset')
+parser.add_argument('--terastation', action='store_true')
 args = parser.parse_args()
 
+if not args.terastation:
+    database_path = '/home/brian/data'
+    duplicate = os.symlink
+else:
+    database_path = '/mnt/terastation/shape_data/MASTER_DB_v1'
+    duplicate = shutil.copyfile # can't make symlinks on terastation
+
 index_db_lines = []
-new_dataset_path = os.path.join(DATABASE_PATH, args.dataset)
+new_dataset_path = os.path.join(database_path, args.dataset)
 new_feature_path = os.path.join(new_dataset_path, 'features')
 
 with open(args.dataset) as f:
@@ -30,7 +37,7 @@ with open(args.dataset) as f:
         key = tokens[1]
         category = '' if len(tokens) == 2 else tokens[2]
 
-        old_dataset_path = os.path.join(DATABASE_PATH, orig_dataset)
+        old_dataset_path = os.path.join(database_path, orig_dataset)
         old_feature_path = os.path.join(old_dataset_path, 'features')
 
         # create symlinks
@@ -40,22 +47,22 @@ with open(args.dataset) as f:
             src = os.path.join(old_dataset_path, fname)
             link_name = '%s_%s' %(orig_dataset, fname)
             dst = os.path.join(new_dataset_path, link_name)
-            os.symlink(src, dst)
+            duplicate(src, dst)
 
         # symlink features
         # src = os.path.join(old_feature_path, key + '.json')
         # dst = os.path.join(new_feature_path, key + '.json')
-        # os.symlink(src, dst)
+        # duplicate(src, dst)
 
         # for src in glob.iglob(os.path.join(old_feature_path, key + '*')):
         #     dst = os.path.join(new_feature_path, os.path.basename(src))
-        #     os.symlink(src, dst)
+        #     duplicate(src, dst)
 
         # add "origdataset_key category" to index_db_lines
         new_key = '%s_%s' %(orig_dataset, key)
         index_db_line = '%s %s\n' %(new_key, category) if category else new_key
         index_db_lines.append(index_db_line)
 
-new_index = os.path.join(DATABASE_PATH, args.dataset, 'index.db')
+new_index = os.path.join(database_path, args.dataset, 'index.db')
 with open(new_index, 'w') as f:
     f.writelines(index_db_lines)
