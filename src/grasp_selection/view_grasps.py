67a1787b016ce82f88f9e4da5eea49224c2f087d
jeff <jmahl42@gmail.com>
2015-09-08 13:41:14 -0700

    Changed the grasp viewer to be up-to-date with working with the latest models for visualization in the Dex-Net paper

--- src/grasp_selection/view_grasps.py
+++ src/grasp_selection/view_grasps.py
@@ -10,10 +10,13 @@ import argparse
 
 import database
 import pr2_grasp_checker as pgc
+import numpy as np
+import IPython
 
 CONFIG = {
-    'database_dir': '/mnt/terastation/shape_data/MASTER_DB_v1/'
+    'database_dir': '/mnt/terastation/shape_data/MASTER_DB_v2/'
 }
+THETA_RES = 2.0 * np.pi / 20.0
 
 def visualize(graspable, grasps):
     """Visualize a list of grasps on a graspable.
@@ -22,20 +25,24 @@ def visualize(graspable, grasps):
     grasps - list of ParallelJawPtGrasp3D or ParallelJawPtPose3D instances
     """
     viewer = pgc.OpenRaveGraspChecker()
-    viewer.view_grasps(graspable, grasps)
+    # viewer.view_grasps(graspable, grasps)
+    viewer.prune_grasps_in_collision(graspable, grasps, delay=1.0)
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('dataset')
     parser.add_argument('graspable')
-    parser.add_argument('grasp_dir')
+    parser.add_argument('--grasp_dir', default = None)
     args = parser.parse_args()
 
     dataset = database.Dataset(args.dataset, CONFIG)
     graspable = dataset[args.graspable]
     grasps = dataset.load_grasps(args.graspable, args.grasp_dir)
 
-    visualize(graspable, grasps)
+    rotated_grasps = []
+    for g in grasps:
+        rotated_grasps.extend(g.transform(graspable.tf, THETA_RES))
+    visualize(graspable, rotated_grasps)
 
 if __name__ == '__main__':
     main()
