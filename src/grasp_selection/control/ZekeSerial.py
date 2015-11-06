7380979dd2c4a31a03e6dbc2f75008fcaa9d9afb
Jacky Liang <jackyliang@berkeley.edu>
2015-11-06 14:24:09 -0800

    Process serial communication working

--- src/grasp_selection/control/ZekeSerial.py
+++ src/grasp_selection/control/ZekeSerial.py
@@ -1,23 +1,23 @@
 import numpy as np
 from serial import Serial
-from multiprocessing import Process, Pipe
-from Queue import Queue
+from multiprocessing import Process, Queue
 from time import sleep, time
 from DexConstants import DexConstants
 
-class ZekeSerial(Process):    
+class _ZekeSerial(Process):    
+    #Private class that abstracts serial communication with Zeke
 
     INIT_DELAY = 3 #magic 3 seconds initial wait for serial connection to stablize
     NUM_STATES = 6
     
-    def __init__(self, flags_pipe, states_pipe, comm, baudrate, timeout):
-        print "hi 1"
+    def __init__(self, flags_q, states_q, state_read_q, comm, baudrate, timeout):
         Process.__init__(self)
         self._comm = comm
         self._baudrate = baudrate
         self._timeout = timeout
-        self._flags_pipe = flags_pipe
-        self._states_pipe = states_pipe
+        self._flags_q = flags_q
+        self._states_q = states_q
+        self._state_read_q = state_read_q
         
         self._flags = {
             "stopping" : False,
@@ -35,28 +35,19 @@ class ZekeSerial(Process):
         self._current_state = self._getStateSerial()
         self._read_state = self._current_state[::]
         
-        self._state_queue = Queue()
-        self._state_queue.put(self._current_state)
+        self._states_q.put(self._current_state)
         while True:
-            print "hi"
-            flags = self._flags_pipe.recv()
-            print "hi"
-            if flags:
-                for flag, value in flags.items():
-                    self._flags[flag] = value
-                    
-            new_state = self._states_pipe.recv()
-            if new_state:
-                #queues new state for the robot to follow
-                self._state_queue.put(state[::])
-                
+            if not self._flags_q.empty():
+                flag = self._flags_q.get();
+                self._flags[flag[0]] = flag[1]
+                                    
             if self._flags["stopping"]:
                 self._stop_robot()
                 self.ser.close()
                 break
 
             if self._flags["reading"]:
-                self._read_state = self._getStateSerial()
+                self._state_read_q.put(self._getStateSerial())
                 self._flags["reading"] = False
             
             #sending the current state
@@ -65,8 +56,8 @@ class ZekeSerial(Process):
             sleep(DexConstants.INTERP_TIME_STEP)
             
             #only update current state if state queue is not empty and we're not pausing
-            if not self._state_queue.empty() and not self._flags["pausing"]:
-                self._current_state = self._state_queue.get()
+            if not self._states_q.empty() and not self._flags["pausing"]:
+                self._current_state = self._states_q.get()
 
     def _isValidState(self, state):
         is_rot = lambda x : x in (0, 3, 5)
@@ -77,9 +68,11 @@ class ZekeSerial(Process):
                 bound = DexConstants.INTERP_MAX_M
             if abs(state[i] - self._current_state[i]) >= bound:
                 return False
-        return True      
+        return True                   
 
     def _sendSingleStateRequest(self, requests):
+        if not self._isValidState(requests):
+            raise Exception("State is invalid or out of bounds for safety reasons")
         self.ser.flushInput()
         self.ser.flushOutput()
         self.ser.write("a")
@@ -111,18 +104,6 @@ class ZekeSerial(Process):
         
     def _stop_robot(self):
         self.control([0,0,0,0,0,0])
-
-    def getState(self):
-        '''
-        Returns current state of the robot by asking the thread to read the state using _getStateSerial
-        '''
-        self._flags["reading"] = True
-        started = time()
-        while self._flags["reading"]:
-            if time() - started > 2:
-                self._flags["reading"] = False
-                raise Exception("Get State timed out")
-        return self._read_state
         
     def _getStateSerial(self):
         self.ser.flushInput()
@@ -139,27 +120,41 @@ class ZekeSerial(Process):
 class ZekeSerialInterface:
     
     def __init__(self, comm = "COM3", baudrate=115200, timeout=.01):
-        flags_pipe_send, flags_pipe_recv = Pipe()
-        states_pipe_send, states_pipe_recv = Pipe()
-        self._zeke_serial = ZekeSerial(flags_pipe_recv, states_pipe_recv, comm, baudrate, timeout)
-        self._flags_pipe = flags_pipe_send
-        self._states_pipe = states_pipe_recv
+        self._comm = comm
+        self._baudrate = baudrate
+        self._timeout = timeout
+        self._reset()
+        
+    def _reset(self):
+        self._flags_q = Queue()
+        self._states_q = Queue()
+        self._state_read_q = Queue()
+        self._zeke_serial = _ZekeSerial(self._flags_q, self._states_q, self._state_read_q, self._comm, self._baudrate, self._timeout)
         
     def start(self):
         self._zeke_serial.start()
         
     def stop(self):
-        self._flags_pipe.send({"stopping", True})
+        self._flags_q.put(("stopping", True))
+        self._reset()
 
     def pause(self):
-        self._flags_pipe.send({"pausing", True})
+        self._flags_q.put(("pausing", True))
             
     def resume(self):
-        self._flags_pipe.send({"resume", True})
+        self._flags_q.put(("resume", True))
+        
+    def getState(self):
+        self._flags_q.put(("reading", True))
+        
+        started = time()
+        while self._state_read_q.empty():
+            if time() - started > 2:
+                raise Exception("Get State timed out")
+
+        return self._state_read_q.get()
         
     def queueState(self, state):
-        if not self._isValidState(state):
-            raise Exception("State is invalid or out of bounds for safety reasons")
-        self._state_pipe.send(state)
+        self._states_q.put(state[::])
         
 z  = ZekeSerialInterface()
