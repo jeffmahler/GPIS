38b2b6eceacc0daef3cd1dfe962a37ae0b205392
Brian Hou <brian.hou@live.com>
2015-11-13 15:01:43 -0800

    Use databases instead of manually loading mesh_file and sdf_file.

--- src/grasp_selection/coverage.py
+++ src/grasp_selection/coverage.py
@@ -13,27 +13,27 @@ import logging
 import time
 
 import contacts
+import database as db
 import graspable_object as go
 import obj_file
 import sdf_file
 
-def load_graspable_and_contacts(mesh_file_name, sdf_file_name, plot=False):
-    # Loading GraspableObject takes ~2 seconds
-    start_loading = time.time()
-    sdf = sdf_file.SdfFile(sdf_file_name).read()
-    mesh = obj_file.ObjFile(mesh_file_name).read()
-    graspable = go.GraspableObject3D(sdf, mesh=mesh)
-    print('Loading GraspableObject took', time.time() - start_loading, 'seconds')
+def load_contacts(graspable, plot=False):
+    sdf = graspable.sdf
+    mesh = graspable.mesh
 
     if plot:
         sdf_surface_points, _ = sdf.surface_points()
+        print(sdf.transform_pt_obj_to_grid(np.array([0, 0, 0])))
+        # sdf_surface_points = sdf_surface_points - 1
         vertex_points = np.array([sdf.transform_pt_obj_to_grid(np.array(v)) for v in mesh.vertices()])
-        plot_sdf_vs_mesh(sdf_surface_points, vertex_points)
+        plot_sdf_vs_mesh(sdf_surface_points, vertex_points, max(sdf.dimensions))
 
     # Loading contacts takes ~16 seconds
     start_loading_contacts = time.time()
     vertex_contacts = []
     on_surface_count = 0
+    IPython.embed()
     for vertex, normal in zip(mesh.vertices(), mesh.normals()):
         contact = contacts.Contact3D(graspable, np.array(vertex), -np.array(normal))
         vertex_contacts.append(contact)
@@ -49,7 +49,7 @@ def load_graspable_and_contacts(mesh_file_name, sdf_file_name, plot=False):
 
     return graspable, vertex_contacts
 
-def plot_sdf_vs_mesh(sdf_surface_points, mesh_surface_points):
+def plot_sdf_vs_mesh(sdf_surface_points, mesh_surface_points, dim=50):
     def plot_surface(points, color):
         x, y, z = points[:, 0], points[:, 1], points[:, 2]
         ax.scatter(x, y, z, c=color)
@@ -61,18 +61,25 @@ def plot_sdf_vs_mesh(sdf_surface_points, mesh_surface_points):
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
-    ax.set_xlim3d(0, 25)
-    ax.set_ylim3d(0, 25)
-    ax.set_zlim3d(0, 25)
+    ax.set_xlim3d(0, dim)
+    ax.set_ylim3d(0, dim)
+    ax.set_zlim3d(0, dim)
 
     plt.show()
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.ERROR)
-    mesh_file_name = '../normalized-google-drill.obj'
-    sdf_file_name = '../normalized-google-drill.sdf'
-    graspable, vertex_contacts = \
-        load_graspable_and_contacts(mesh_file_name, sdf_file_name, plot=True)
+    database_filename = 'google_test_db.hdf5'
+    config = {
+        'database_dir': '.',
+        'database_cache_dir': '.',
+        'datasets': {
+            'google': {'start_index': 0, 'end_index': 1}
+        }
+    }
+    database = db.Hdf5Database(database_filename, config)
+    graspable = database['google']['normalized-google-drill']
+    vertex_contacts = load_contacts(graspable, plot=True)
     for contact in vertex_contacts:
         contact.plot_friction_cone()
         IPython.embed()
