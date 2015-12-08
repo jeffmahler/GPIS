ef5583f769b0e374d9d482ac9c7dab39bbf11961
jeff <jmahl42@gmail.com>
2015-12-08 15:08:27 -0800

    Fixed bugs in the stp computation that included but may not have been limited to: (a) incorrect barycentric coords (b) not checking the orientation of the object (up or down) (c) not zeroing out unstable faces. Also added inertia computation to the mesh classs

---
+++ src/grasp_selection/cv_hull_test.py
@@ -0,0 +1,58 @@
+import numpy as np
+import mesh
+import obj_file as of
+import stable_poses as stp
+
+import similarity_tf as stf
+import tfx
+
+import pyhull
+import pyhull.convex_hull as cvh
+import os
+import sys
+
+import mayavi.mlab as mv
+
+import IPython
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    ofile = of.ObjFile(filename)
+    msh = ofile.read()
+    cvh_msh = msh.convex_hull()
+
+    min_prob = 0.01
+    prob_mapping = stp.compute_stable_poses(msh)
+    R_list = []
+    for face, p in prob_mapping.items():
+        if p >= min_prob:# and face[0] == 161 and face[1] == 16 and face[2] == 67:
+            R_list.append([p, stp.compute_basis([cvh_msh.vertices()[i] for i in face], msh), face])
+
+    #    m.num_connected_components()
+    for R in R_list:
+        tf = stf.SimilarityTransform3D(pose=tfx.pose(R[1]))
+        m = msh.transform(tf)
+            
+        mv.figure()
+        m.visualize()
+        mv.points3d(m.center_of_mass[0], m.center_of_mass[1], m.center_of_mass[2], scale_factor=0.01)
+        mv.points3d(m.vertex_mean_[0], m.vertex_mean_[1], m.vertex_mean_[2], scale_factor=0.01, color=(1,0,0))
+        mv.axes()
+#    mv.points3d(cvh_m.center_of_mass_[0], cvh_m.center_of_mass_[1], cvh_m.center_of_mass_[2], scale_factor=0.01)
+#    mv.points3d(cvh_m.vertex_mean_[0], cvh_m.vertex_mean_[1], cvh_m.vertex_mean_[2], scale_factor=0.01, color=(1,0,0))
+
+        cvh_m = cvh_msh.transform(tf)
+        mv.figure()
+        cvh_m.visualize()
+        mv.points3d(cvh_m.center_of_mass_[0], cvh_m.center_of_mass_[1], cvh_m.center_of_mass_[2], scale_factor=0.01)
+        mv.points3d(cvh_m.vertex_mean_[0], cvh_m.vertex_mean_[1], cvh_m.vertex_mean_[2], scale_factor=0.01, color=(1,0,0))
+
+        vertices = np.array(cvh_m.vertices())
+        v = np.array([vertices[i] for i in R[2]])
+        mv.points3d(v[:,0], v[:,1], v[:,2], scale_factor=0.01, color=(0,1,0))
+
+        mv.axes()
+        mv.show()
+
+        #IPython.embed()
+ 
