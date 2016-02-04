38225d85c860228118fb01838468a5823948c4be
Jeff Mahler <jmahler@rll4.(none)>
2016-02-04 14:54:08 -0800

    working setup on jeff's computer for testing temp_jacky.py

--- src/grasp_selection/temp_jacky.py
+++ src/grasp_selection/temp_jacky.py
@@ -4,14 +4,15 @@ import numpy as np
 import database as db
 import experiment_config as ec
 
-config_file = "/jacky_working/GPIS/cfg/test_hdf5_label_grasps_gce_jacky.yaml"
-config = ec.ExperimentConfig(config_file)
+config_file = "../../cfg/test_hdf5_label_grasps_gce_jacky.yaml"
+database_filename = "/mnt/terastation/shape_data/MASTER_DB_v3/dexnet_db3_01_22_16.hdf5"
+dataset_name = "YCB"
 
-database_filename = " /mnt/terastation/shape_data/Master_DB_v3"
+config = ec.ExperimentConfig(config_file)
 database = db.Hdf5Database(database_filename, config)
 
 # read the grasp metrics and features
-ds = database.dataset(args.dataset)
+ds = database.dataset(dataset_name)
 o = ds.object_keys
 grasps = ds.grasps(o[0])
 grasp_features = ds.grasp_features(o[0], grasps)
