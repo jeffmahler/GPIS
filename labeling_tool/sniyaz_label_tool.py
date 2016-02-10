341ec0ff9163ea0179c495ddb044961b5322f53a
“sniyaz” <sniyaz@berkeley.edu>
2016-02-09 16:44:08 -0800

    Got coloring working, as well as support for grabbing triangles with one vertex projected into bounding box, or onlt those that have ALL verticies projcected. Also fixed issue of label mode indicator dissapearing after a selection.

--- labeling_tool/sniyaz_label_tool.py
+++ labeling_tool/sniyaz_label_tool.py
@@ -1,28 +1,3 @@
-"""
-This example shows how to use a callback to select a red ball amongst white
-balls.
-
-The example uses the figure method 'on_mouse_pick', added in Mayavi
-3.4, to register a callback when the left mouse is pressed. The callback
-is called with a picker, enabling to identify the object selected.
-Specificaly, actors are selected, each object is represented on the scene
-via actors. The selected actors can be found in 'picker.actors'. In this
-example, we have plotted red balls and white ball. We want to select the
-red balls, and thus test if any actor in picker.actors corresponds to an
-actor of red balls.
-
-To identify which ball has been selected, we use the point id. However,
-each ball is represented by several points. Thus we need to retrieve the
-number of points per ball, and divide the point id by this number.
-
-We use an outline to display which ball was selected by positioning it on
-the corresponding ball.
-"""
-
-# Author: Gael Varoquaux <gael dot varoquaux at normalesup.org>
-# Copyright (c) 2009, Enthought, Inc.
-# License: BSD style.
-
 import numpy as np
 from mayavi import mlab
 import time
@@ -31,6 +6,15 @@ import pdb
 from tvtk.api import tvtk
 import pdb
 import mlab_3D_to_2D
+import os
+import sys
+
+other_code_dir = "../src/grasp_selection"
+other_code_dir = os.getcwd() + "/" + other_code_dir
+sys.path.insert(0, other_code_dir)
+
+import obj_file as of
+import mesh
 
 ################################################################################
 # Disable the rendering, to get bring up the figure quicker:
@@ -38,35 +22,24 @@ figure = mlab.gcf()
 mlab.clf()
 figure.scene.disable_render = True
 
-# Creates two set of points using mlab.points3d: red point and
-# white points
-x1, y1, z1 = np.random.random((3, 10))
-red_glyphs = mlab.points3d(x1, y1, z1, color=(1, 0, 0),
-                resolution=20)
+#Load mesh here
 
-x2, y2, z2 = np.random.random((3, 10))
-white_glyphs = mlab.points3d(x2, y2, z2, color=(0.9, 0.9, 0.9),
-                resolution=20)
+filename = "/Users/Sherdil/Research/GPIS/data/test/meshes/Co_clean.obj"
+ofile = of.ObjFile(filename)
+msh = ofile.read()
+colored_msh = None
 
-# Add an outline to show the selected point and center it on the first
-# data point.
-outline = mlab.outline(line_width=3)
-outline.outline_mode = 'full'
-outline.bounds = (x1[0]-0.1, x1[0]+0.1,
-                  y1[0]-0.1, y1[0]+0.1,
-                  z1[0]-0.1, z1[0]+0.1)
+
+msh.visualize((0.0, 0.0, 1.0))
+
+
 
 # Every object has been created, we can reenable the rendering.
 figure.scene.disable_render = False
 ################################################################################
 
 
-# Here, we grab the points describing the individual glyph, to figure
-# out how many points are in an individual glyph.
-glyph_points = red_glyphs.glyph.glyph_source.glyph_source.output.points.to_array()
 
-
-################################################################################
 
 #Sherdil Code start
 
@@ -141,18 +114,27 @@ def select_mode(vtk_obj, event):
 
 def project_3D(box_start, box_end):
 
-    W = np.ones(x1.shape)
-    hmgns_world_coords = np.column_stack((x1, y1, z1, W))
+    global msh
+    global colored_msh
+
+    x = np.array([i[0] for i in msh.vertices()])
+    y = np.array([i[1] for i in msh.vertices()])
+    z = np.array([i[2] for i in msh.vertices()])
+    W = np.ones(x.shape)
+    triangles = msh.triangles()
+
+    hmgns_world_coords = np.column_stack((x, y, z, W))
     comb_trans_mat = mlab_3D_to_2D.get_world_to_view_matrix(figure.scene)
     view_coords = mlab_3D_to_2D.apply_transform_to_points(hmgns_world_coords, comb_trans_mat)
     norm_view_coords = view_coords / (view_coords[:, 3].reshape(-1, 1))
     view_to_disp_mat = mlab_3D_to_2D.get_view_to_display_matrix(figure.scene)
     disp_coords = mlab_3D_to_2D.apply_transform_to_points(norm_view_coords, view_to_disp_mat)
 
-    for i in range(len(x1)):
+    in_vertex_indicies = []
+    for i in range(len(x)):
 
         screen_x_cord = disp_coords[:, 0][i]
-        #For some reason, the script we stole think the origin is the upper left. It's actually the 
+        #For some reason, the script we stole thinks the origin is the upper left. It's actually the 
         #lower left, so we need to adjust here!
         screen_y_cord = figure.scene.get_size()[1] - disp_coords[:, 1][i]
         
@@ -160,36 +142,37 @@ def project_3D(box_start, box_end):
         y_in_box = screen_y_cord > min(box_start[1], box_end[1]) and screen_y_cord < max(box_start[1], box_end[1])
 
         if x_in_box and y_in_box:
-            outline.bounds = (x1[i]-0.1, x1[i]+0.1,y1[i]-0.1, y1[i]+0.1, z1[i]-0.1, z1[i]+0.1)
-            break
+            in_vertex_indicies.append(i)
 
 
-    
-    #print(original_mouse_position)
+    #Label any triangle with A (even one) vertex in the bounding box
+    #removed_triangles = [t for t in triangles if len(set(t).intersection(in_vertex_indicies)) != 0]
+    #new_triangles = [t for t in triangles if len(set(t).intersection(in_vertex_indicies)) == 0]
 
-    #def end_draw_box():
+    #Label only triangles that have EVERY vertex in the bounding box.
+    removed_triangles = [t for t in triangles if len(set(t).intersection(in_vertex_indicies)) == 3]
+    new_triangles = [t for t in triangles if len(set(t).intersection(in_vertex_indicies)) < 3]
 
 
-    #interactor.add_observer('KeyPressEvent', your_function)
+    msh = mesh.Mesh3D(msh.vertices(), new_triangles, msh.normals())
+    if not colored_msh and len(removed_triangles) > 0:
+        colored_msh = mesh.Mesh3D(msh.vertices(), removed_triangles, msh.normals())
+    else:
+        colored_msh = mesh.Mesh3D(msh.vertices(), removed_triangles + colored_msh.triangles(), msh.normals())
 
+    mlab.clf()
+    msh.visualize((0.0, 0.0, 1.0))
+    if colored_msh:
+        colored_msh.visualize((1.0, 0.0, 0.0))
 
-
-    # while(True):
-    #     current_mouse_posiiton = interactor.event_position
-    #     print(current_mouse_posiiton)
-    #     wx.Yield()
-        
-        
-        
-
+    mlab.title('LABEL MODE ON')
 
 
 
 interactor.add_observer('LeftButtonPressEvent', box_bounding)
 interactor.add_observer('MouseMoveEvent', draw_box)
 interactor.add_observer('KeyPressEvent', select_mode)
-# Decrease the tolerance, so that we can more easily select a precise
-# point.
+
 
 
 
