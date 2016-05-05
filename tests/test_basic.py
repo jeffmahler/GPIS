cff7475712da77446b1432e215766a0bb39c630c
Brian Hou <brian.hou@live.com>
2016-05-04 18:04:07 -0700

    Add a test case to check dependencies.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,3 +3,8 @@ import unittest
 class BasicTestCase(unittest.TestCase):
     def test_passing(self):
         self.assertEqual(True, True)
+
+class DependencyTestCase(unittest.TestCase):
+    def test_numpy(self):
+        import numpy
+        print(numpy.version.version)
