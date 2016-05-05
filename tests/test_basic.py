35c78074827caa1239175407aab51747050bac8e
Brian Hou <brian.hou@live.com>
2016-05-04 18:54:51 -0700

    Update requirements.txt.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -6,8 +6,22 @@ class BasicTestCase(unittest.TestCase):
     def test_passing(self):
         self.assertEqual(True, True)
 
-class DependencyTestCase(unittest.TestCase):
-    def test_numpy(self):
+class ImportTestCase(unittest.TestCase):
+    def test_core(self):
+        import ipython
+        import matplotlib
         import numpy
-        print()
-        print('[numpy]', numpy.version.version)
+        import scipy
+
+    def test_imaging(self):
+        import PIL
+        import skimage
+        import sklearn
+
+    def test_experiments(self):
+        import h5py
+        import yaml
+
+    def test_misc(self):
+        import nearpy
+        import sklearn
